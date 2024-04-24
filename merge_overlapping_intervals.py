ba468428754c743aa21a5536eb4c9da01c3de3b1
Ajit Deshpande <ajitsdeshpande@gmail.com>
2024-04-24 16:29:34 +0100

    Add files via upload
    Merge overlapping intervals ad return the result having intervals after merging

---
+++ merge_overlapping_intervals.py
@@ -0,0 +1,38 @@
+'''
+merge any overlapping intervals 
+
+'''
+
+
+def merge_intervals(intervals): 
+
+    # Sort the intervals by their start time 
+
+    sorted_intervals = sorted(intervals, key=lambda x: x[0]) 
+
+    # Initialize the merged list with the first interval 
+    merged = [sorted_intervals[0]] 
+
+    for current in sorted_intervals: 
+        # Get the last element in the merged list 
+        last = merged[-1]
+        print(f"current is {current}")
+        print(f"last is {last}")
+
+        # If the current interval overlaps with the last one, merge them 
+        if current[0] <= last[1]: 
+            last[1] = max(last[1], current[1]) #when we change last[1] it changes the corresponding value of merged[-1]
+            print(f"last: modified to {last}")
+        else: 
+            # Add the current interval to the merged list 
+            merged.append(current) 
+            print(f"merged is {merged}")
+
+    return merged 
+ 
+
+# Test the function 
+
+intervals = [[1, 3], [5, 12], [4, 10], [20, 25]] 
+
+print(merge_intervals(intervals)) 
